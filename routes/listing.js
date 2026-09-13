5d54a0788cd8cf54630d0c4b96f60f8f372fc62a
Kaveri Sahu <kaverisahu376@gmail.com>
2026-09-14 00:58:27 +0530

    Add logout middleware

--- routes/listing.js
+++ routes/listing.js
@@ -4,6 +4,9 @@ const wrapAsync = require("../utils/wrapAsync.js");
 const ExpressError = require("../utils/ExpressError.js")
 const {listingSchema} = require("../schema.js");
 const Listing = require("../models/listing.js");
+const { isLoggedIn } = require("../middleware.js");
+
+
 
 
 
@@ -28,7 +31,8 @@ router.get("/", wrapAsync(async (req, res) => {
 }));
 
 //New Route
-router.get("/new", (req, res) => {
+router.get("/new", isLoggedIn, (req, res) => {
+  console.log(req.user);
   res.render("listings/new.ejs");
 });
 
@@ -44,7 +48,7 @@ router.get("/:id", wrapAsync(async (req, res) => {
 }));
 
 //Create Route
-router.post("/",
+router.post("/",isLoggedIn, 
   validateListing,
    wrapAsync(async (req, res) => {
     const newListing = new Listing(req.body.listing);
@@ -55,7 +59,7 @@ router.post("/",
 );
 
 //Edit Route
-router.get("/:id/edit", wrapAsync(async (req, res) => {
+router.get("/:id/edit",isLoggedIn,  wrapAsync(async (req, res) => {
   let { id } = req.params;
   const listing = await Listing.findById(id);
   if(!listing) {
@@ -66,7 +70,7 @@ router.get("/:id/edit", wrapAsync(async (req, res) => {
 }));
 
 //Update Route
-router.put("/:id",
+router.put("/:id",isLoggedIn, 
   validateListing,
    wrapAsync(async (req, res) => {
   let { id } = req.params;
@@ -76,7 +80,7 @@ router.put("/:id",
 }));
 
 //Delete Route
-router.delete("/:id", wrapAsync(async (req, res) => {
+router.delete("/:id",isLoggedIn,  wrapAsync(async (req, res) => {
   let { id } = req.params;
   let deletedListing = await Listing.findByIdAndDelete(id);
   console.log(deletedListing);
