97f90551ffc560d775d7fa18207e08753968ea32
Kaveri Sahu <kaverisahu376@gmail.com>
2026-09-14 14:51:20 +0530

    add Owner

--- init/index.js
+++ init/index.js
@@ -18,8 +18,13 @@ async function main() {
 
 const initDB = async () => {
   await Listing.deleteMany({});
+  initData.data = initData.data.map((Obj) => ({
+    ...obj,
+    owner: ""}))
   await Listing.insertMany(initData.data);
   console.log("data was initialized");
 };
 
 initDB();
+
+
--- routes/user.js
+++ routes/user.js
@@ -2,7 +2,8 @@ const express = require("express");
 const router = express.Router();
 const User = require("../models/user.js");
 const wrapAsync = require("../utils/wrapAsync.js");
-const passport = require("passport")
+const passport = require("passport");
+const { saveRedirectUrl } = require("./middleware.js");
 
 router.get("/signup", (req,res)=>{
     res.render("users/signup.ejs")
@@ -14,8 +15,13 @@ router.get("/signup", wrapAsync(async (req,res)=>{
         const newUser = new User({email, username});
         const registeredUser = await User.register(newUser, password);
         console.log(registeredUser)
-        req.flash("success", "Welcome to Wanderlust");
-        res.redirect("/listings");
+        req.login(registeredUser, (err) => {
+            if(err) {
+                return next(err)
+            }
+            req.flash("success", "Welcome to Wanderlust");
+            res.redirect(req.session.redirectUrl);
+        })
     } catch(err) {
         req.flash("error", e.message);
         res.redirect("/signup");
@@ -27,9 +33,15 @@ router.get("/login", (req,res)=>{
     res.render("users/login.ejs")
 });
 
-router.post("/login", passport.authenticate("local", {failureRedirect: '/login', failureFlash: true}) ,async(req, res)=>{
-   res.flash("success","Welcome back to Wanderlust");
-   res.redirect("/listings");
+router.post("/login", 
+    saveRedirectUrl, 
+    passport.authenticate("local", 
+        {failureRedirect: '/login',
+             failureFlash: true}),
+             async(req, res)=>{
+                res.flash("success","Welcome back to Wanderlust");
+                let redirectUrl = res.locals.redirectUrl || "/listings";
+                res.redirect(redirectUrl);
 })
 
 router.get("/logout", (req,res)=>{
