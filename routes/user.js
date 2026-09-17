399399b41a4ecc9b439c0ecb43ad8be7ebc3c892
Kaveri Sahu <kaverisahu376@gmail.com>
2026-09-17 18:52:45 +0530

    Router Route user file

--- routes/user.js
+++ routes/user.js
@@ -6,18 +6,32 @@ const passport = require("passport");
 const { saveRedirectUrl } = require("./middleware.js");
 const userController = require("../controllers/users.js");
 
-router.get("/signup", userController.renderSignupForm)
+router.route("/signup")
+.get(userController.renderSignupForm)
+.get(wrapAsync(userController.signup));
 
-router.get("/signup", wrapAsync(userController.signup));
-
-router.get("/login", userController.renderLoginForm);
-
-router.post("/login", 
+router.route("/login")
+.get(userController.renderLoginForm)
+.post(
     saveRedirectUrl, 
     passport.authenticate("local", 
         {failureRedirect: '/login',
              failureFlash: true}),
              userController.login)
+
+
+// router.get("/signup", userController.renderSignupForm)
+
+// router.get("/signup", wrapAsync(userController.signup));
+
+// router.get("/login", userController.renderLoginForm);
+
+// router.post("/login", 
+//     saveRedirectUrl, 
+//     passport.authenticate("local", 
+//         {failureRedirect: '/login',
+//              failureFlash: true}),
+//              userController.login)
 
 router.get("/logout", userController.logout)
 
