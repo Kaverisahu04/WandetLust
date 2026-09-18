cb31bd4b588bbe2c80aa5fc14dd25b976b952dbf
Kaveri Sahu <kaverisahu376@gmail.com>
2026-09-18 13:00:40 +0530

    cloudinary storage system

--- routes/listing.js
+++ routes/listing.js
@@ -5,7 +5,8 @@ const Listing = require("../models/listing.js");
 const { isLoggedIn, isOwner, validateListing } = require("../middleware.js");
 const listingController = require("../controllers/listing.js")
 const multer = require('multer');
-const upload = multer({dest: 'uploads/'})
+const {storage} = require("../cloudConfig.js")
+const upload = multer({storage})
 
 
 router
@@ -15,7 +16,6 @@ router
 //   validateListing,
 //    wrapAsync(listingController.createListing)
 // );
-
 .post(upload.single('listing[image]'), (req, res)=>{
   res.send(req.file)
 })
